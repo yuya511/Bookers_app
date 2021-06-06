bef05d361a85641e3c5eaadb1a8ce469b02d346a
yuya511 <20015114452y@gmail.com>
2021-06-06 15:17:02 +0000

    Almost completed

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to "/books/#{book.id}"
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to "/books/#{book.id}"
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+  get 'books' => 'books#index', as: 'books'
+  get 'books/:id' => 'books#show'
+  post 'books' => 'books#create'
+  get 'books/:id/edit' => 'books#edit'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
 end
