76486782d47cc6d6c7d27fe7dacbdb45c9344eda
yuya511 <20015114452y@gmail.com>
2021-06-07 16:55:01 +0000

    error message ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,33 +3,37 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to "/books/#{book.id}"
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to "/books/#{book.id}"
+    else
+      @books = Book.all
+      render action: :index
+    end
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to "/books/#{book.id}"
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title,:body)
